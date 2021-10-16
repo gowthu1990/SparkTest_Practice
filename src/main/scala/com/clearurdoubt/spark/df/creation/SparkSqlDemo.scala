d291c99f5e6a70b62b832d839e12ab9a02af7bb1
gowth <gowtam.badvity@gmail.com>
2021-10-16 20:25:21 +0100

    moving import to object level

--- src/main/scala/com/clearurdoubt/spark/df/creation/SparkSqlDemo.scala
+++ src/main/scala/com/clearurdoubt/spark/df/creation/SparkSqlDemo.scala
@@ -5,16 +5,13 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object SparkSqlDemo extends SparkInitializer {
   implicit val spark: SparkSession = getSparkSession(master = "local[*]", appName = "SparkDemo")
+  import spark.implicits._
 
   def createSimpleDF(collection: Seq[Int]): DataFrame = {
-    import spark.implicits._
-
     collection.toDF("id")
   }
 
   def createTuple3DF(collection: Seq[(Int, String, String)]): DataFrame = {
-    import spark.implicits._
-
     collection.toDF("id", "first_name", "last_name")
   }
 }
