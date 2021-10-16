44469588e05dc70374c781971343ab35343b126e
gowth <gowtam.badvity@gmail.com>
2021-10-16 18:38:25 +0100

    Basic DataFrame creation and Unit Test cases

--- src/test/scala/com/clearurdoubt/spark/part01/SparkSqlDemoSpec.scala
+++ src/test/scala/com/clearurdoubt/spark/part01/SparkSqlDemoSpec.scala
@@ -1,8 +1,23 @@
 package com.clearurdoubt.spark.part01
 
+import org.apache.spark.sql.{DataFrame, Row}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class SparkSqlDemoSpec extends AnyFlatSpec with Matchers {
-  ""
+  val list = List(1, 2, 3)
+  val df: DataFrame = SparkSqlDemo.createDF(list)
+
+  "createDF()" should "create a DataFrame with one column \"id\"" in {
+    val columns = df.columns.toList
+
+    columns.length shouldBe 1
+    columns.head shouldBe "id"
+  }
+
+  it should "create a DataFrame with three rows" in {
+    val actual = df.collect()
+
+    actual shouldBe Array(Row(1), Row(2), Row(3))
+  }
 }
